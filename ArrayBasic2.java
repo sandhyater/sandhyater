cb4e0e965263b1dddad8a65b3bcc3bb3a59801a6
sandhyater <sandhya.terala@gmail.com>
2024-04-17 10:25:13 +0530

    All are array programms

---
+++ ArrayBasic2.java
@@ -0,0 +1,26 @@
+package arrayprograms;
+
+import java.util.Arrays;
+
+public class ArrayBasic2 {
+
+	public static void main(String[] args) {
+		int[] a=new int[5];
+		a[0]=25;
+		a[1]=7;
+		a[2]=52;
+		a[3]=56;
+		a[4]=89;
+		for(int i=0;i<5;i++)
+		{
+	System.out.println(a[i]);
+	
+		} 
+		//short cut to print values
+
+		/*Arrays.toString(a);
+		System.out.println(		Arrays.toString(a));*/
+
+
+	}
+}
---
+++ ArrayList_Program.java
@@ -0,0 +1,26 @@
+package arrayprograms;
+import java.util.ArrayList;
+public class ArrayList_Program 
+{
+
+public static void main(String[] args) 
+{
+ArrayList a1=new ArrayList();
+a1.add(987);
+a1.add(908);
+a1.add("Manish");
+a1.add(true);
+a1.add(8.65);
+a1.add('P');
+System.out.println(a1);	
+ArrayList a2=new ArrayList();	
+a2.add(1);
+a2.add(9);
+a2.add("sandhya");
+a2.add(false);
+a2.add(22235655.2);
+a2.add('s');
+System.out.println(a2);	
+	
+}
+}
